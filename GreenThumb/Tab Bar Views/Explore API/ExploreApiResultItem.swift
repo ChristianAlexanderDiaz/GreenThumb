f88774581816dfdbb8b1d382cbe59621c283c2b4
ClassyCynical <98622372+ClassyCynical@users.noreply.github.com>
2023-04-11 15:07:24 -0400

    Testing the branch
    Added Details, Item, List to test the branch

---
+++ GreenThumb/Tab Bar Views/Explore API/ExploreApiResultItem.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreApiResultItem.swift
+//  GreenThumb
+//
+//  Created by Christian Alexander Diaz on 4/11/23.
+//  Copyright © 2023 Taylor Adeline Flieg, Christian Alexander Diaz, Brian Andrew Wood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExploreApiResultItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExploreApiResultItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreApiResultItem()
+    }
+}
---
+++ GreenThumb/Tab Bar Views/Explore API/ExploreApiResultsList.swift
@@ -0,0 +1,25 @@
+//
+//  ExploreApiResultsList.swift
+//  GreenThumb
+//
+//  Created by Christian Alexander Diaz on 4/11/23.
+//  Copyright © 2023 Taylor Adeline Flieg, Christian Alexander Diaz, Brian Andrew Wood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExploreApiResultsList: View {
+    var body: some View {
+        Form {
+            Section(header: Text("Plant Name")) {
+                Text("Example Plant Name")
+            }
+        }
+    }
+}
+
+struct ExploreApiResultsList_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreApiResultsList()
+    }
+}
